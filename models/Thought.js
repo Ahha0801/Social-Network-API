d33c2738388a060acffcb3abf32b1e13b14fecb9
Ahha0801 <128229173+Ahha0801@users.noreply.github.com>
2023-05-25 09:23:21 -0500

    set up reactionSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction')
+const reactionSchema = require('./reactionSchema')
 const dateFormat = require('../utils/helpers')
 
 const thoughtSchema = new Schema(
