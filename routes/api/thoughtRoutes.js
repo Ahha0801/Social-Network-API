d726efb5faab33190e46ce951aecd6878fcf973f
Ahha0801 <128229173+Ahha0801@users.noreply.github.com>
2023-05-25 09:31:57 -0500

    set up routes to work for users thoughts and reactions

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,20 +1,24 @@
 const router = require('express').Router();
 const {
-  getCourses,
-  getSingleCourse,
-  createCourse,
-  updateCourse,
-  deleteCourse,
-} = require('../../controllers/courseController.js');
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController');
 
-// /api/courses
-router.route('/').get(getCourses).post(createCourse);
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
 
-// /api/courses/:courseId
-router
-  .route('/:courseId')
-  .get(getSingleCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+// /api/thoughts/:id
+router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/users/:id/thoughts/:thoughtId/reactions
+router.route('/:id/thoughts/:thoughtId/reactions').post(addReaction)
+
+// /api/users/:id/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:id/thoughts/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,16 +3,17 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser,
   deleteUser,
   addFriend,
   removeFriend,
-} = require('../../controllers/studentController');
+} = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:id
-router.route('/:id').get(getSingleUser).delete(deleteUser);
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 // /api/users/:id/friends/:friendId
 router.route('/:id/friends/:friendId').post(addFriend).delete(removeFriend);
